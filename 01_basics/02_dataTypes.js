284ba6979c6b4111f7c469c73f6cc72110ce9605
Ashray Stan <stanashray@gmail.com>
2024-11-25 15:14:28 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Ashray"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number - 2 to power 53
+// bigint
+// string - ""
+// boolean - true / false
+// null - standalone value
+// undefined - value not assigned
+// symbol - unique
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
