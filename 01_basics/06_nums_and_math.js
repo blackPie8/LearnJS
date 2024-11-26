1f2135d1c049fa4fcdca786d9784dde242bf9f09
Ashray Stan <stanashray@gmail.com>
2024-11-26 08:43:05 +0000

    Small changes in math and nums

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -30,6 +30,6 @@ const hundreds = 1000000
 // console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
-const max = 30
+const max = 12
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
