59a006a620a68aebe012f22a367b9b06a5b4ad3f
Ashray Stan <stanashray@gmail.com>
2024-11-28 05:03:37 +0000

    contd on objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// const tinderUser = new Object()    // Singleton Object
+
+const tinderUser = {}  // Not SingleTon
+
+tinderUser.name  = "Ashray"
+tinderUser.age = 21
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "ashray@gmail.com",
+    fullname : {
+        userfullname : {
+        firstname : "Ashray",
+        lastname : "Stan"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = { 1:"a", 2:"b"}
+const obj2 = { 3:"c", 4:"d"}
+
+// const obj3 = {obj1, obj2}
+// const obj3  = Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+
+const obj4 = {...obj1,...obj2}
+// console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
